a5bede4b363abe94b2a65b88b8d6ac045f20d212
huykhoong <ihnep11@gmail.com>
2020-04-24 11:48:39 +0700

    ''

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ require_once 'vendor/autoload.php';
 
 $app = new Slim\App();
 
+$app->get('/', function(){
+
+});
+
 $app->post('/rewrite', function(\Slim\Http\Request $request, \Slim\Http\Response $response){
     $response_params = [
         'success' => false,
@@ -11,32 +15,31 @@ $app->post('/rewrite', function(\Slim\Http\Request $request, \Slim\Http\Response
         'data' => []
     ];
 
-    if( ! $request->getContentType() == 'application/json'){
-        $response_params['message'] = "Request Content-Type must be 'application/json'";
+    $allGetVars = $request->getQueryParams();
+    var_dump($allGetVars);
+    exit();
+
+    $request_params = $request->getParsedBody();
+
+    if(empty($request_params)){
+        $response_params['message'] = "Request params missing";
+    }
+    else if( ! isset($request_params['text']) OR empty($request_params['text'])){
+        $response_params['message'] = "Request params 'text' is empty or missing";
     }
     else{
-        $request_params = $request->getParsedBody();
+        $request_text = $request_params['text'];
+//            $request_text = preg_replace('/[^0-9a-z-\.,\?!\(\)\s\n\r]/i', '', $request_text);
+        $request_include_capitalized = isset($response_params['include_capitalized']) ? $response_params['include_capitalized'] : false;
+        $rewritten_text = spin_article($request_text, $request_include_capitalized);
 
-        if(empty($request_params)){
-            $response_params['message'] = "Request params missing";
-        }
-        else if( ! isset($request_params['text']) OR empty($request_params['text'])){
-            $response_params['message'] = "Request params 'text' is empty or missing";
+        if( ! $rewritten_text){
+            $response_params['message'] = 'Rewriting failed';
         }
         else{
-            $request_text = $request_params['text'];
-//            $request_text = preg_replace('/[^0-9a-z-\.,\?!\(\)\s\n\r]/i', '', $request_text);
-            $request_include_capitalized = isset($response_params['include_capitalized']) ? $response_params['include_capitalized'] : false;
-            $rewritten_text = spin_article($request_text, $request_include_capitalized);
-
-            if( ! $rewritten_text){
-                $response_params['message'] = 'Rewriting failed';
-            }
-            else{
-                $response_params['success'] = true;
-                $response_params['message'] = 'Text rewritten successfully';
-                $response_params['data']['rewritten_text'] = $rewritten_text;
-            }
+            $response_params['success'] = true;
+            $response_params['message'] = 'Text rewritten successfully';
+            $response_params['data']['rewritten_text'] = $rewritten_text;
         }
     }
 
